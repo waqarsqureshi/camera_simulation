40643e4bfdc584cb4fa34586d0bbc6cd6f7bef83
waqarsqureshi <waqar.shahid@gmail.com>
2018-08-07 09:30:47 +0800

    update code

--- includes/learnopengl/camera.h
+++ includes/learnopengl/camera.h
@@ -12,15 +12,21 @@ enum Camera_Movement {
     FORWARD,
     BACKWARD,
     LEFT,
-    RIGHT
+    RIGHT,
+    UP_ARROW,
+    DOWN_ARROW,
+    LEFT_ARROW,
+    RIGHT_ARROW,
+    UP,
+    DOWN   
 };
 
 // Default camera values
-const float YAW         = -90.0f;
+const float YAW         = 90.0f;
 const float PITCH       =  0.0f;
-const float SPEED       =  2.5f;
-const float SENSITIVITY =  0.1f;
-const float ZOOM        =  45.0f;
+const float SPEED       =  0.3f;//2.5f
+const float SENSITIVITY =  0.2f;
+const float ZOOM        =  90.20f;// camera FOV of Phantom 4 DJI
 
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
@@ -67,17 +73,38 @@ public:
     }
 
     // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
-    void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime,GLboolean constrainPitch = true)
     {
-        float velocity = MovementSpeed * deltaTime;
+        float velocity = MovementSpeed ;//* deltaTime;
         if (direction == FORWARD)
-            Position += Front * velocity;
+            Position.z  = Position.z + velocity;
         if (direction == BACKWARD)
-            Position -= Front * velocity;
+            Position.z  = Position.z - velocity;
         if (direction == LEFT)
-            Position -= Right * velocity;
+            Position.x  = Position.x - velocity;
         if (direction == RIGHT)
-            Position += Right * velocity;
+            Position.x  = Position.x + velocity;
+        if (direction == UP)
+            Position.y  = Position.y + velocity;
+        if (direction == DOWN)
+            Position.y  = Position.y - velocity;
+        if (direction == UP_ARROW)
+            Pitch = Pitch + velocity;
+        if (direction == DOWN_ARROW)
+            Pitch = Pitch - velocity;
+        if (direction == RIGHT_ARROW)
+            Yaw  = Yaw + velocity;
+        if (direction == LEFT_ARROW)
+            Yaw  = Yaw - velocity;            
+        // Make sure that when pitch is out of bounds, screen doesn't get flipped
+        if (constrainPitch)
+        {
+            if (Pitch > 89.0f)
+                Pitch = 89.0f;
+            if (Pitch < -89.0f)
+                Pitch = -89.0f;
+        }
+        updateCameraVectors();
     }
 
     // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
@@ -86,8 +113,8 @@ public:
         xoffset *= MouseSensitivity;
         yoffset *= MouseSensitivity;
 
-        Yaw   += xoffset;
-        Pitch += yoffset;
+        Yaw   += xoffset;// pane
+        Pitch += yoffset;// tilt
 
         // Make sure that when pitch is out of bounds, screen doesn't get flipped
         if (constrainPitch)
@@ -105,12 +132,12 @@ public:
     // Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
     void ProcessMouseScroll(float yoffset)
     {
-        if (Zoom >= 1.0f && Zoom <= 45.0f)
+        if (Zoom >= 1.0f && Zoom <= ZOOM)
             Zoom -= yoffset;
         if (Zoom <= 1.0f)
             Zoom = 1.0f;
-        if (Zoom >= 45.0f)
-            Zoom = 45.0f;
+        if (Zoom >= ZOOM)
+            Zoom = ZOOM;
     }
 
 private:
